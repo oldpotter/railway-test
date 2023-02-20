306a97be74ce4b14440caa3ce2f9abc8b38424ad
沈克令 <9315193+shen-keling@user.noreply.gitee.com>
2023-02-20 11:53:15 +0800

    ss

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from flask import Flask, jsonify
+from flask_restful import Resource, Api
 import os
 
 app = Flask(__name__)
+api = Api(app)
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+# @app.route('/')
+# def index():
+#     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+
+    # from flask import Flask
+# from flask_restful import Resource, Api
+ 
+# app = Flask(__name__)
+# api = Api(app)
+ 
+# class HelloWorld(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+ 
+# api.add_resource(HelloWorld, '/')
+ 
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000)
