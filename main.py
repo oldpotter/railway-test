fc857352a414095d6ac8220cf7448fd74759ad94
沈克令 <9315193+shen-keling@user.noreply.gitee.com>
2023-02-20 11:58:30 +0800

    b

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 import os
+import asyncio
+from bilibili_api import video, sync
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,7 +12,13 @@ api = Api(app)
 #     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+         # 实例化 Video 类
+        v = video.Video(bvid="BV1uv411q7Mv")
+        # 获取信息
+        info = sync(v.get_info())
+        # 打印信息
+        print(info)
+        return {'hello': info}
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
